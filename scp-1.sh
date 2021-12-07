4dfe3db6a29475b0896656f79228f3dcce7ebb1f
ShaoFuLiu <zx22920416@gmail.com>
2021-12-07 21:50:34 +0800

    20211207

---
+++ scp-1.sh
@@ -0,0 +1,3 @@
+rsync -rzvh --progress liusf@scopion.phys.nthu.edu.tw:/home/liusf/tSDRG/Sorting_data/Spin1/metadata/* ./Spin1_new/metadata/
+
+
---
+++ scp-2.sh
@@ -0,0 +1,3 @@
+rsync -rzvh --progress liusf@scopion.phys.nthu.edu.tw:/home/liusf/tSDRG/Sorting_data/Spin2/metadata/* ./Spin2_new/metadata/
+
+
